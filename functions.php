ac44739c8950c124cb896cc6e69529885071d4fc
El Richo <ricgarridoc@hotmail.com>
2020-10-07 18:21:11 -0500

    Actualizando

--- functions.php
+++ functions.php
@@ -29,16 +29,25 @@ function gisea_prototype_scripts_style(){
     wp_enqueue_style('slicknav',get_template_directory_uri() . '/css/slicknav.css',array(),'1.0.0');
 
     wp_enqueue_style('googleFont','https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&family=Raleway:wght@400;700;900&family=Staatliches&display=swap"',array(),'1.0.0');
+    //is_page('aquí va el slug'): para que no tenga que descargar la galería(libreria) en cada una de las páginas
     
+    wp_enqueue_style('lightboxCSS',get_template_directory_uri() . '/css/lightbox.css',array(),'2.11.3');
+    
+        
+    
+
     # HOJA DE ESTILOS PRINCIPAL
     # el array() vacio es donde van las dependencias y se deja vacio cuando no las haya
     # Es importante poner una version ya que si instalas un plugin de cache y haces modificaciones, con que le pongas 1.0.1, te mostrara las modificaciones
     wp_enqueue_style('style',get_stylesheet_uri(),array('normalize','googleFont'),'1.0.0');
 
-
+    
     //Así se cargan archivos JavaScript
     wp_enqueue_script('queryslicknav',get_template_directory_uri() . '/js/jquery.slicknav.min.js',array('jquery'),'1.0.0',true);
     wp_enqueue_script('scripts',get_template_directory_uri() . '/js/scripts.js',array('jquery','queryslicknav'),'1.0.0',true);
+    
+    wp_enqueue_script('lightboxJS',get_template_directory_uri() . '/js/lightbox.js',array('jquery'),'2.11.3',true);
+    
 }
 //Este hook quiere decir que va a cargar hojas de estilo en la parte frontal de la web
 add_action('wp_enqueue_scripts','gisea_prototype_scripts_style');
--- header.php
+++ header.php
@@ -11,7 +11,7 @@
         <div class="contenedor">
             <div class="barra-navegacion">
                 <div class="logo">
-                    <img src="<?php echo get_template_directory_uri();?>/img/logo.svg" alt="logo sitio">
+                    <img src="<?php echo get_template_directory_uri();?>/img/logo para pag blanco 2.svg" alt="logo sitio">
                 </div>
 
                 <?php
