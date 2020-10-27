3a1481b85f7a4309f3b7dc551d10284aa8d166b7
El Richo <ricgarridoc@hotmail.com>
2020-10-27 16:08:42 -0600

    new css styles

--- front-page.php
+++ front-page.php
@@ -5,4 +5,62 @@
     <p><?php the_field('texto_bienvenida'); ?></p>
 </section>
 
+
+<div class="seccion-areas">
+    <ul class="contenedor-areas">
+        <li class="area">
+            <?php 
+                $area1 = get_field('area_1');
+                //Extrayendo la imagen usando el name del subfield que esta en "custom fields" y diciendole el tamaño
+                $imagen = wp_get_attachment_image_src($area1['area_imagen'], 'mediano')[0];
+                //var_dump($area1);
+            ?>
+            <img src="<?php echo esc_attr($imagen); ?>" alt="">
+            <p><?php echo esc_html($area1['area_texto']); ?></p>
+        </li>
+
+        <li class="area">
+            <?php 
+                $area2 = get_field('area_2');
+                //Extrayendo la imagen usando el name del subfield que esta en "custom fields" y diciendole el tamaño
+                $imagen = wp_get_attachment_image_src($area2['area_imagen'], 'mediano')[0];
+                //var_dump($area1);
+            ?>
+            <img src="<?php echo esc_attr($imagen); ?>" alt="">
+            <p><?php echo esc_html($area2['area_texto']); ?></p>
+        </li>
+
+        <li class="area">
+            <?php 
+                $area3 = get_field('area_3');
+                //Extrayendo la imagen usando el name del subfield que esta en "custom fields" y diciendole el tamaño
+                $imagen = wp_get_attachment_image_src($area3['area_imagen'], 'mediano')[0];
+                //var_dump($area1);
+            ?>
+            <img src="<?php echo esc_attr($imagen); ?>" alt="">
+            <p><?php echo esc_html($area3['area_texto']); ?></p>
+        </li>
+
+        <li class="area">
+            <?php 
+                $area4 = get_field('area_4');
+                //Extrayendo la imagen usando el name del subfield que esta en "custom fields" y diciendole el tamaño
+                $imagen = wp_get_attachment_image_src($area4['area_imagen'], 'mediano')[0];
+                //var_dump($area1);
+            ?>
+            <img src="<?php echo esc_attr($imagen); ?>" alt="">
+            <p><?php echo esc_html($area4['area_texto']);?></p>
+            
+        </li>
+    </ul>
+</div>
+
+<section class="testimoniales">
+    <h2 class="text-center texto-blanco">Testimoniales</h2>
+    
+    <div class="contenedor-testimoniales">
+       
+    </div>
+</section>
+
 <?php get_footer(); ?>
